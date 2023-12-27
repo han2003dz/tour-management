ad0a319bde46302c3ac9aca8436baf16a4a3dc02
han2003dz <han20031605@gmail.com>
2023-12-27 22:30:48 +0700

    cấu hình pug

---
+++ .history/index_20231227222717.ts
@@ -0,0 +1,12 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/tours", (req: Request, res: Response) => {
+  res.send("Danh sách tour");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
---
+++ .history/index_20231227222822.ts
@@ -0,0 +1,15 @@
+import express, { Express, Request, Response } from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.get("/tours", (req: Request, res: Response) => {
+  res.send("Danh sách tour");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
